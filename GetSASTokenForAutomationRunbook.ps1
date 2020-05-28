a184dfba6d721dcde470caaea934ae0248bc4fd9
MahatoSampa <34264191+MahatoSampa@users.noreply.github.com>
2020-05-28 10:57:28 +0530

    Add files via upload

---
+++ GetSASTokenForAutomationRunbook.ps1
@@ -0,0 +1,62 @@
+{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "artifactsSASToken": {
+      "type": "securestring",
+      "metadata": {
+        "description": "Value of the secret from Key Vault."
+      }
+    },
+    "secretArtifactsLocation": {
+      "type": "string",
+      "defaultValue": ""
+    },
+	"aacName": {
+      "type": "string",
+      "defaultValue": ""
+    },
+    "aacLocation": {
+      "type": "string",
+      "defaultValue": ""
+    },
+    "runbookName": {
+      "type": "string",
+      "defaultValue": ""
+    },
+    "runbookDescription": {
+      "type": "string",
+      "defaultValue": ""
+    },
+    "powershellScriptFileName": {
+      "type": "string",
+      "defaultValue": ""
+    }
+  },
+  "variables": {
+
+  },
+  "resources": [
+    {
+      "type": "Microsoft.Automation/automationAccounts/runbooks",
+      "apiVersion": "2015-01-01-preview",
+      "name": "[concat(parameters('aacName'),'/',parameters('runbookName'))]",
+      "location": "[parameters('aacLocation')]",
+      "dependsOn": [
+      ],
+      "tags": {},
+      "properties": {
+        "runbookType": "PowerShell",
+        "logProgress": "false",
+        "logVerbose": "false",
+        "description": "[parameters('runbookDescription')]",
+        "publishContentLink": {
+          "uri": "[uri(parameters('secretArtifactsLocation'), concat(parameters('powershellScriptFileName'), parameters('artifactsSASToken')))]",
+          "version": "1.0.0.0"
+        }
+      }
+    }
+  ],
+  "outputs": {
+  }
+}
